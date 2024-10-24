21d521187e081d5b9e4b97736007e5de9b5606e4
adrian-miranda <adrian.miranda.r@gmail.com>
2024-10-24 01:17:24 -0300

    se cambia estructura json, se prueban maps en OpcionMenu.jsx

--- src/components/atoms/OpcionMenu.jsx
+++ src/components/atoms/OpcionMenu.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import json from '../../prueba.json'
 
 export const OpcionMenu = () => {
-    // let datos = json;
+    let datos = json.otra_llave;
     let arreglo = [1 , 2, 3]
+    let arreglo2 = [{"hola" : "chao0"}, {"hola" : "chao1"},{"hola" : "chao2"} , {"dia" : "noche"}]
     return (
         <div>
             {Object.keys(json).map(key => (
@@ -11,13 +12,31 @@ export const OpcionMenu = () => {
                     {key}: {JSON.stringify(json[key])}
                 </div>
             ))}
+
+            <ul>
+                {arreglo.map((a)=> {
+                   return(
+                    <li>{a}</li>
+                   )
+                })}
+            </ul>
+            {arreglo2.map(a2 =>{
+                return(
+                        <ul>
+                            <li><h3>{a2.dia}</h3></li>
+                        </ul>
+                )
+            })}
             {
-                arreglo.map(a=> a)
+                datos.map((j)=>{
+                    return(
+                        <ul>
+                            <li>{j}</li>
+                        </ul>
+                    )
+                })
             }
-{/* 
-            {
-                datos.map(j => j)
-            } */}
+
         </div>
     );
 }
