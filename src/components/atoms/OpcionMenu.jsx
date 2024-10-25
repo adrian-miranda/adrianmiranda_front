a642f0ff146d5676df16968d0ab8f141a886ef71
adrian-miranda <adrian.miranda.r@gmail.com>
2024-10-24 23:48:23 -0300

    se modifica .gitignore ya que se generaron archivos txt de apoyo, se modifica opcion menu para mapear el contenido que le llegue para la barra de navegacion , se mejora prueba.json para estructurarlo mejor

--- src/components/atoms/OpcionMenu.jsx
+++ src/components/atoms/OpcionMenu.jsx
@@ -2,41 +2,17 @@ import React from 'react'
 import json from '../../prueba.json'
 
 export const OpcionMenu = () => {
-    let datos = json.otra_llave;
-    let arreglo = [1 , 2, 3]
-    let arreglo2 = [{"hola" : "chao0"}, {"hola" : "chao1"},{"hola" : "chao2"} , {"dia" : "noche"}]
     return (
         <div>
-            {Object.keys(json).map(key => (
-                <div key={key}>
-                    {key}: {JSON.stringify(json[key])}
-                </div>
-            ))}
-
-            <ul>
-                {arreglo.map((a)=> {
-                   return(
-                    <li>{a}</li>
-                   )
-                })}
-            </ul>
-            {arreglo2.map(a2 =>{
+            {json && json.length > 0 ? json.map((js , index)=>{
                 return(
-                        <ul>
-                            <li><h3>{a2.dia}</h3></li>
-                        </ul>
+                    <ul>
+                        <li key={index}>{index} {js.llave}</li>
+                    </ul>
                 )
-            })}
-            {
-                datos.map((j)=>{
-                    return(
-                        <ul>
-                            <li>{j}</li>
-                        </ul>
-                    )
-                })
+            })
+            :  "no hay datos"
             }
-
         </div>
     );
 }
