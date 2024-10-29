e3b4e6bca71e4addde219be2aefbf667744c0c65
adrian-miranda <adrian.miranda.r@gmail.com>
2024-10-28 23:11:45 -0300

    se usa axios, useEffect y useState para conectar localmente una api que esta en otro servidor a modo de prueba

--- src/components/atoms/OpcionMenu.jsx
+++ src/components/atoms/OpcionMenu.jsx
@@ -1,18 +1,41 @@
-import React from 'react'
-import json from '../../prueba.json'
+import React, { useEffect, useState } from 'react'
+// import json from '../../prueba.json'
+import axios from 'axios';
 
 export const OpcionMenu = () => {
+    
+    const [personas , setPersonas] = useState([]);
+    
+    useEffect(()=>{
+        const obtenerPersonas = async ()=>{
+            const url = 'http://127.0.0.1:8000/api/personas/';
+            const response = await axios.get(url)
+            setPersonas(response.data)
+        }
+        obtenerPersonas()
+    },[]);
+    
     return (
-        <div>
-            {json && json.length > 0 ? json.map((js , index)=>{
-                return(
-                    <ul>
-                        <li key={index}>{index} {js.llave}</li>
-                    </ul>
-                )
-            })
-            :  "no hay datos"
-            }
-        </div>
+        <>
+            {/* <div>
+                {json && json.length > 0 ? json.map((js , index)=>{
+                    return(
+                        <ul key={index}>
+                            <li key={index}>{index} {js.llave}</li>
+                        </ul>
+                    )
+                })
+                :  "no hay datos"
+                }
+            </div> */}
+            <div>
+                <ul>
+                    {personas && personas.length > 0 ?  personas.map((persona , index)=>(
+                        <li key={index}>{persona.apellido_paterno}</li>
+                    )) : 'no hay data'}
+                </ul>
+            </div>
+        
+        </>
     );
 }
