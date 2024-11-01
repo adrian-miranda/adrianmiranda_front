3f2b9a4cd5f59e2e1366cf78cf3968a3d864d63f
adrian-miranda <adrian.miranda.r@gmail.com>
2024-10-31 21:48:45 -0300

    creando navbar

--- src/components/organisms/NavBar.tsx
+++ src/components/organisms/NavBar.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import style from './navbar.module.css'
+import logo from '../../assets/images/logo_adrian_miranda_final.png'
 
 export const NavBar = () => {
   return (
-    <div className={style.navbarcuerpo}>NavBar</div>
+    <div className={style.navbarcuerpo}>
+      <img src={logo} alt="la imagen"  className={style.logo}/>
+      <h1>AdrianMiranda.cl</h1>
+      <ul className={style.ul}>
+        <li className={style.lista}>Quien soy</li>
+        <li className={style.lista}>Servicios</li>
+        <li className={style.lista}>Curruculum</li>
+        <li className={style.lista}>Casos de exito</li>
+        <li className={style.lista}>Contacto</li>
+      </ul>
+    </div>
   )
 }
