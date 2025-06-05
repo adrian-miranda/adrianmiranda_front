710f04b59b7100ac1f3741ebea66d9efaa0322bf
adrian-miranda <adrian.miranda.r@gmail.com>
2025-06-04 20:24:26 -0400

    se entiende como usar tailwind + style.module juntos

--- src/components/organisms/NavBar.jsx
+++ src/components/organisms/NavBar.jsx
@@ -4,38 +4,12 @@ import ItemList from '../molecules/ItemList/ItemList'
 import style from './navbar.module.css'
 
 
-/* const NavBar = () => {
-return (
-    <div className={style.ancho}>
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-            <Logo/>
-            <button     className="navbar-toggler" 
-                        type="button" 
-                        data-bs-toggle="collapse" 
-                        data-bs-target="#navbarSupportedContent" 
-                        aria-controls="navbarSupportedContent" 
-                        aria-expanded="false" 
-                        aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <ItemList/>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    </div>
-    )
-} */
-
 const NavBar = () => {
 return (
-    <>
+    <div className={`bg-blue-500 p-4 ${style.ancho}`}>
         <Logo/>
         <ItemList/>
-    </>
+    </div>
 
     )
 }
